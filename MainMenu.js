4aa11467c1ba62ccc2966352c3e2303981f02f78
Romildo Juliano <rjal@cin.ufpe.br>
2021-01-11 18:44:44 -0300

    Add files via upload

--- MainMenu.js
+++ MainMenu.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react';
-import { Dimensions, StyleSheet, Text, View, TextInput, KeyboardAvoidingView } from 'react-native';
+import { 
+  Dimensions, 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from 'react-native';
+import { Button } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 import LottieView from 'lottie-react-native';
 import { useFonts } from 'expo-font';
@@ -16,68 +25,86 @@ const LoadingScreen = () => {
         Copyright © 2021 Silent Spell. All Rights Reserved
       </Text>
     </View>
-    
   );
 }
 
 const LoginForm = () => {
   return (
-    <View style={styles.animatedView}>
-      <KeyboardAvoidingView
-      style={{ flex: 1 }}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 40}
-      >
-        <Text style={styles.textStyle}>
-          Login
-        </Text>
-        
-        <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05 }]}>
-          Insira seu email
-        </Text>
-        <TextInput 
-          allowFontScaling
-          autoCapitalize='none'
-          autoCompleteType='email'
-          autoCorrect={false}
-          caretHidden
-          clearButtonMode='while-editing'
-          keyboardType='email-address'
-          onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
-          returnKeyType='done'
-          selectionColor='purple'
-          textAlign='center'
-          textContentType='emailAddress'
-          style={styles.inputStyle}
-        />
+      <View style={styles.animatedView}>
+        <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 40}
+        >
+          <Text style={styles.textStyle}>
+            Login
+          </Text>
+          
+          <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05 }]}>
+            Email
+          </Text>
+          <TextInput 
+            allowFontScaling
+            autoCapitalize='none'
+            autoCompleteType='email'
+            autoCorrect={false}
+            caretHidden
+            clearButtonMode='while-editing'
+            keyboardType='email-address'
+            onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
+            returnKeyType='done'
+            selectionColor='purple'
+            textAlign='center'
+            textContentType='emailAddress'
+            style={styles.inputStyle}
+          />
 
-        <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05, paddingTop: 30 }]}>
-          Insira sua senha
-        </Text>
-        <TextInput 
-          allowFontScaling
-          autoCapitalize='none'
-          autoCompleteType='password'
-          autoCorrect={false}
-          caretHidden
-          clearButtonMode='while-editing'
-          keyboardType='default'
-          onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
-          returnKeyType='go'
-          selectionColor='purple'
-          textAlign='center'
-          secureTextEntry
-          textContentType='password'
-          style={styles.inputStyle}
-        />
-      </KeyboardAvoidingView>
-    </View>
-    
+          <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05, paddingTop: 30 }]}>
+            Senha
+          </Text>
+          <TextInput 
+            allowFontScaling
+            autoCapitalize='none'
+            autoCompleteType='password'
+            autoCorrect={false}
+            caretHidden
+            clearButtonMode='while-editing'
+            keyboardType='default'
+            onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
+            returnKeyType='go'
+            selectionColor='purple'
+            textAlign='center'
+            secureTextEntry
+            textContentType='password'
+            style={styles.inputStyle}
+          />
+
+        </KeyboardAvoidingView>
+      </View>
+  );
+}
+
+const LoginButton = () => {
+  return (
+    <TouchableOpacity style={styles.containerButton}>
+      <Button 
+        title='ENTRAR'
+        buttonStyle={styles.buttonStyle}
+        titleStyle={{ 
+          fontFamily: 'OpenSans-Bold', 
+          fontWeight: 'bold', 
+          textShadowColor: 'rgba(0, 0, 0, 1)',
+          textShadowOffset: { width: 0, height: 1 },
+          textShadowRadius: 1, 
+        }}
+      />
+    </TouchableOpacity>
   );
 }
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    'LakkiReddy-Regular': require('./assets/LakkiReddy-Regular.ttf')
+    'LakkiReddy-Regular': require('./assets/LakkiReddy-Regular.ttf'),
+    'OpenSans-Bold': require('./assets/OpenSans-Bold.ttf'),
   });
 
   const [flag, setFlag] = useState(0);
@@ -93,12 +120,52 @@ export default function App() {
       return LoadingScreen();
     }
     else if (flag == 1) {
-      return LoginForm();
+      return (
+        <View>
+          {LoginForm()}
+          {LoginButton()}
+          <TouchableOpacity style={styles.createAccountTextContainer}>
+            <Text style={styles.createAccountText}>
+              Não possui uma conta? Clique aqui
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
     }
   }
 }
 
 const styles = StyleSheet.create({
+  createAccountTextContainer: {
+    flex: 1, 
+    position: 'absolute', 
+    alignItems: 'center', 
+    alignSelf: 'center', 
+    alignContent: 'center', 
+    marginTop: Dimensions.get('window').height * .8
+  },
+  createAccountText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: Dimensions.get('window').width * .04,
+    textDecorationLine: 'underline'
+  },
+  buttonStyle: {
+    width: Dimensions.get('window').width * .6,
+    height: Dimensions.get('window').height * .075,
+    borderRadius: 30,
+    backgroundColor: '#FF6F61',
+    borderBottomWidth: 2,
+    borderBottomColor: '#000'
+  },
+  containerButton: { 
+    flex: 1, 
+    position: 'absolute', 
+    alignItems: 'center', 
+    alignSelf: 'center', 
+    alignContent: 'center',
+    marginTop: Dimensions.get('window').height * .565,
+  },
   inputStyle:{
     // Box related
     width: Dimensions.get('window').width * .85,
@@ -146,9 +213,13 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   bottomPageText: {
+    fontFamily: 'OpenSans-Bold',
     textAlignVertical: 'bottom',
     paddingTop: Dimensions.get('window').height * .9,
     color: '#FFFFFF',
-    position: 'absolute'
+    position: 'absolute',
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 5,
   }
 });
