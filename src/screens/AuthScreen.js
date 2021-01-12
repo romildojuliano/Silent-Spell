1dcf5931199d284ac505a6097b53dfd4525f3057
romildojuliano <rjal@cin.ufpe.br>
2021-01-12 01:04:28 -0300

    -d

---
+++ src/screens/AuthScreen.js
@@ -0,0 +1,226 @@
+import React, { useState } from 'react';
+import { 
+  Dimensions, 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from 'react-native';
+import { Button } from 'react-native-elements';
+import * as Animatable from 'react-native-animatable';
+import LottieView from 'lottie-react-native';
+import { useFonts } from 'expo-font';
+
+const LoadingScreen = () => {
+  return (
+    <View style={styles.animatedView}>
+      <Animatable.Text animation='pulse' easing='ease-out' iterationCount='infinite' style={styles.animatableText}>
+        <Text style={styles.textStyle}>Silent Spell</Text>
+        <LottieView source={require('../../assets/main_screen_loading.json')} loop autoPlay autoSize={false} style={{ width: 100, height: 100, flex: 1, alignSelf: 'center', paddingBottom: 50, position: 'absolute' }} />
+      </Animatable.Text>
+
+      <Text style={styles.bottomPageText}>
+        Copyright © 2021 Silent Spell. All Rights Reserved
+      </Text>
+    </View>
+  );
+}
+
+const LoginForm = () => {
+  return (
+      <View style={styles.animatedView}>
+        <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        keyboardVerticalOffset={Platform.OS === 'ios' ? 40 : 40}
+        >
+          <Text style={styles.textStyle}>
+            Login
+          </Text>
+          
+          <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05 }]}>
+            Email
+          </Text>
+          <TextInput 
+            underlineColorAndroid="transparent"
+            allowFontScaling
+            autoCapitalize='none'
+            autoCompleteType='email'
+            autoCorrect={false}
+            caretHidden
+            clearButtonMode='while-editing'
+            keyboardType='email-address'
+            onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
+            returnKeyType='done'
+            selectionColor='purple'
+            textAlign='center'
+            textContentType='emailAddress'
+            style={styles.inputStyle}
+          />
+
+          <Text style={[styles.textStyle, { fontSize: Dimensions.get('window').height * .05, paddingTop: 30 }]}>
+            Senha
+          </Text>
+          <TextInput 
+            underlineColorAndroid="transparent"
+            allowFontScaling
+            autoCapitalize='none'
+            autoCompleteType='password'
+            autoCorrect={false}
+            caretHidden
+            clearButtonMode='while-editing'
+            keyboardType='default'
+            onSubmitEditing={() => {}} // Função realizada ao pressionar buttão 'Enter'
+            returnKeyType='go'
+            selectionColor='purple'
+            textAlign='center'
+            secureTextEntry
+            textContentType='password'
+            style={styles.inputStyle}
+          />
+
+        </KeyboardAvoidingView>
+      </View>
+  );
+}
+
+const LoginButton = () => {
+  return (
+    <TouchableOpacity style={styles.containerButton}>
+      <Button 
+        title='ENTRAR'
+        buttonStyle={styles.buttonStyle}
+        titleStyle={{ 
+          fontFamily: 'OpenSans-Bold', 
+          fontWeight: 'bold', 
+          textShadowColor: 'rgba(0, 0, 0, 1)',
+          textShadowOffset: { width: 0, height: 1 },
+          textShadowRadius: 1, 
+        }}
+      />
+    </TouchableOpacity>
+  );
+}
+
+export default function AuthScreen() {
+  let [fontsLoaded] = useFonts({
+    'LakkiReddy-Regular': require('../../assets/LakkiReddy-Regular.ttf'),
+    'OpenSans-Bold': require('../../assets/OpenSans-Bold.ttf'),
+  });
+
+  const [flag, setFlag] = useState(0);
+  
+  if (!fontsLoaded) {
+    return <></>;
+  }
+  else {
+    if (flag == 0) {
+      setInterval(() => {
+        setFlag(1);
+      }, 7500);
+      return LoadingScreen();
+    }
+    else if (flag == 1) {
+      return (
+        <View>
+          {LoginForm()}
+          {LoginButton()}
+          <TouchableOpacity style={styles.createAccountTextContainer}>
+            <Text style={styles.createAccountText}>
+              Não possui uma conta? Clique aqui
+            </Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  createAccountTextContainer: {
+    flex: 1, 
+    position: 'absolute', 
+    alignItems: 'center', 
+    alignSelf: 'center', 
+    alignContent: 'center', 
+    marginTop: Dimensions.get('window').height * .8
+  },
+  createAccountText: {
+    color: '#ffffff',
+    fontWeight: 'bold',
+    fontSize: Dimensions.get('window').width * .04,
+    textDecorationLine: 'underline'
+  },
+  buttonStyle: {
+    width: Dimensions.get('window').width * .6,
+    height: Dimensions.get('window').height * .075,
+    borderRadius: 30,
+    backgroundColor: '#FF6F61',
+    borderBottomWidth: 2,
+    borderBottomColor: '#000'
+  },
+  containerButton: { 
+    flex: 1, 
+    position: 'absolute', 
+    alignItems: 'center', 
+    alignSelf: 'center', 
+    alignContent: 'center',
+    marginTop: Dimensions.get('window').height * .565,
+  },
+  inputStyle:{
+    // Box related
+    width: Dimensions.get('window').width * .85,
+    height: Dimensions.get('window').height * .07,
+    alignContent: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#dbdfef',
+    borderBottomWidth: 2,
+    borderRadius: 10,
+    // Font Related
+    fontFamily: 'LakkiReddy-Regular', 
+    fontSize: Dimensions.get('window').height * .03,
+    textAlignVertical: 'bottom',
+    color: '#564c26',
+  },
+  textStyle: {
+    fontFamily: 'LakkiReddy-Regular', 
+    fontSize: 59, 
+    textAlign: 'center', 
+    color: '#fc9e21',
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 5,
+  },
+  animatedView: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('screen').height * 1.25,
+    justifyContent: 'center', 
+    //alignContent: 'center', 
+    alignItems: 'center',
+    //alignContent: 'center',
+    backgroundColor: '#263056',
+    paddingTop: Dimensions.get('window').height * .045
+  },
+  animatableText: {
+    textAlign: 'center', 
+    textShadowColor: '#000', 
+    textShadowRadius: 2
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomPageText: {
+    fontFamily: 'OpenSans-Bold',
+    textAlignVertical: 'bottom',
+    paddingTop: Dimensions.get('window').height * .9,
+    color: '#FFFFFF',
+    position: 'absolute',
+    textShadowColor: 'rgba(0, 0, 0, 1)',
+    textShadowOffset: { width: 0, height: 1 },
+    textShadowRadius: 5,
+  }
+});
---
+++ src/screens/TrackHandsScreen.js
@@ -0,0 +1,234 @@
+// Módulos do React Native
+import React, { useState } from 'react'
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  Dimensions
+} from 'react-native'
+import { Button } from 'react-native-elements';
+
+// Módulos do Tensorflow.js
+import * as tf from '@tensorflow/tfjs'
+import { cameraWithTensors, detectGLCapabilities } from '@tensorflow/tfjs-react-native'
+import * as handpose from '@tensorflow-models/handpose'
+require('@tensorflow/tfjs-backend-webgl'); // Necessário?
+
+// Módulos do Expo
+import { Camera } from 'expo-camera';
+import Constants from 'expo-constants'
+import * as Permissions from 'expo-permissions'
+
+// Componente de alta ordem para usar as funções da câmera 
+const TensorCamera = cameraWithTensors(Camera);
+
+// Dimensões do aparelho
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class TrackHandsScreen extends React.Component {
+  
+  state = {
+    isTfReady: false,                   // Determina se o módulo do TensorFlow está carregado
+    isModelReady: false,                // Determina se o modelo do @tensorflow-models/handpose está carregado     
+    hasPermission: null,                // Determina se o usuário concedeu permissão ao acesso das cameras
+    type: Camera.Constants.Type.front,  // Define o tipo de câmera padrão que será usada na aplicação
+    showTensorCamera: false,            // Define se a câmera será mostrada na tela
+    frameCounter: 0
+  }
+
+  /*
+  Solicita o acesso à câmera de forma assícrona;
+  Caso não seja garantido, o app não irá funcionar;
+  */
+  getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY)
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!')
+      }
+    }
+  }
+
+  async componentDidMount() {
+    // Altera o estado para indicar que o módulo do TensorFlow está carregado
+    await tf.ready()
+    this.setState({ isTfReady: true })
+
+    // Altera o estado para indicar que o modelo do Handpose está carregado 
+    this.model = await handpose.load()
+    this.setState({ isModelReady: true })
+
+    // Solicita permissão do usuário para ter acesso às câmeras
+    this.getPermissionAsync()
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    // Determina se o usuário garantiu permissão de acesso às câmeras ou não
+    this.setState({ hasPermission: status === 'granted' });
+
+  }
+
+  handleCameraStream = (images, updatePreview, gl) => {
+    console.log(detectGLCapabilities(gl));
+     
+    const loop = async () => {
+    
+      const nextImageTensor = await images.next().value;
+      
+      if (this.state.frameCounter % 10 == 0) { 
+        const predictions = await this.model.estimateHands(nextImageTensor);
+        console.log(predictions)
+      }
+      
+      
+      /* 
+      Funções utilizadas pelo Tensorflow para atualizar os frames da 
+      câmera na tela do celular. O UpdatePreview atualiza o frame,
+      enquanto que o gl.endFrameEXP procoessa o próximo frame.
+      */
+      updatePreview();
+      gl.endFrameEXP();
+
+      this.setState({ frameCounter: this.state.frameCounter + 1 });
+      console.log(this.state.frameCounter);
+
+      // Função que recebe o proóximo frame e retorna ao início do loop
+      requestAnimationFrame(loop);
+    };
+    loop();
+  }
+
+  renderTensorCamera(textureDims, tensorDims) {
+    return (
+      <View>
+          <Text style={styles.detectionText} >
+            Estimating hands. Check log at the console.
+          </Text>
+          <TensorCamera
+            // Standard Camera props
+            style={styles.tfCameraView}
+            type={this.state.type}
+            // Tensor related props
+            cameraTextureHeight={textureDims.height}
+            cameraTextureWidth={textureDims.width}
+            resizeHeight={tensorDims.height}
+            resizeWidth={tensorDims.width}
+            resizeDepth={3}
+            onReady={this.handleCameraStream}
+            autorender={false}
+          />
+        </View>
+    )
+  }
+
+  loadingScreen(isTfReady, isModelReady, hasPermission) {
+    return (
+        <View style={{ marginTop: SCREEN_HEIGHT * .5 }}>
+          { 
+            (isTfReady) ? 
+            <Text style={styles.loaded}>TensorFlow module loaded</Text>
+            :
+            <Text style={styles.notLoaded}>Loading TensorFlow module...</Text>
+          }
+
+          { 
+            (isModelReady) ? 
+            <Text style={styles.loaded}>@mediapipe/handpose loaded</Text>
+            :
+            <Text style={styles.notLoaded}>Loading @mediapipe/handpose model...</Text>
+          }
+
+          { 
+            (hasPermission) ? 
+            <Text style={styles.loaded}>Permissions granted</Text>
+            :
+            <Text style={styles.notLoaded}>Waiting for permissions...</Text>
+          }
+          
+        </View>
+    );
+  }
+
+  render() {
+    const textureDims = (Platform.OS === 'ios') ? { height: 1920, width: 1080} : { height: 1400, width: 1400 };
+    const tensorDims = { width: 320, height: 320 };
+
+    const { isTfReady, isModelReady, hasPermission, showTensorCamera } = this.state;
+
+    if (hasPermission === true) {
+      if (showTensorCamera) {
+        //Carrega o componente do TensorCamera e permite a visualização câmera se showTensor === true
+        return (
+          <View>
+            <Button 
+              title='Stop tracking'
+              style='outline'
+              onPress={() => this.setState({ showTensorCamera: false })}
+              buttonStyle={styles.buttonStyle}
+            />
+            {this.renderTensorCamera(textureDims, tensorDims)}
+          </View>
+        )
+      }
+      else {
+        // Desativa a TensorCamera até que o botão seja pressionado; inicia nesse estado por padrão
+        return (
+          <Button 
+            title='Start tracking'
+            style='outline'
+            onPress={() => this.setState({ showTensorCamera: true })}
+            buttonStyle={styles.buttonStyle}
+          />
+        );
+      }      
+    } 
+    
+    else {
+      // Tela de carregamento inicial
+      return this.loadingScreen(isTfReady, isModelReady, hasPermission);
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  tfCameraView: {
+    width: SCREEN_WIDTH * .75,
+    height: SCREEN_HEIGHT * .5 ,
+    zIndex: 1,
+    borderWidth: 0,
+    borderRadius: 0,
+    alignSelf: 'center',
+    alignContent: 'center',
+    justifyContent: 'center'
+  },
+  loaded: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'green'
+  },
+  notLoaded: {
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'red'
+  },
+  detectionText: { 
+    textAlign: 'center', 
+    textAlignVertical: 'center', 
+    fontWeight: 'bold', 
+    color: 'blue' 
+  },
+  fluffyDetected: {
+    textAlign: 'center', 
+    textAlignVertical: 'center', 
+    fontWeight: 'bold', 
+    color: 'blue' 
+  },
+  buttonStyle: {
+    width: SCREEN_WIDTH * .5,
+    height: SCREEN_HEIGHT * .05,
+    marginTop: SCREEN_HEIGHT * .25, 
+    alignSelf: 'center'
+  }
+})
+
+export default TrackHandsScreen;
