fa0f37a647aec2bc14cd04e4b70bebfe48e7e704
João <Peixoto>
2021-01-26 13:15:51 -0300

    servidor socketio e nome das variaveis em Game.py alterado

--- Servidor/Game.py
+++ Servidor/Game.py
@@ -1,14 +1,15 @@
-import Player
+from Player import Player
 import random
 
-class Magia:
+class Spell:
     def __init__(self,confidence,tipo,element):
         self.confidence = confidence
-        if tipo == 0:
+        self.tipo = tipo
+        if tipo == 'A':
             self.attack = True
-            self.defesa = False
-        elif tipo == 1:
-            self.defesa = True
+            self.defense = False
+        elif tipo == 'D':
+            self.defense = True
             self.attack = False
         self.element = element
 
@@ -16,93 +17,91 @@ class Game:
     def __init__(self,player1Id,player2Id):
         self.player1 = Player(100.0,player1Id)
         self.player2 = Player(100.0,player2Id)
-        magias = 'EILUW LUCBE BLICB UWLCE CLUWI IBLBC'
-        magias = magias.split(' ')
-        trigramas = []
-        for spell in magias:
+        spells = 'EILUW LUCBE BLICB UWLCE CLUWI IBLBC'
+        spells = spells.split(' ')
+        trigramss = []
+        for spell in spells:
             aux = []
             aux.append('_'+spell[0:2])
             for i in range(1,len(spell)-1):
                 aux.append(spell[i-1:i+2])
             aux.append(spell[len(spell)-2:len(spell)] + '_')
-            trigramas.append(aux)
-        self.trigramas = trigramas
-        self.magias = magias
+            trigramss.append(aux)
+        self.trigramss = trigramss
+        self.spells = spells
 
-    def decodificaMagia(self,x):
-        trigrama = []
-        trigrama.append('_'+x[0:2])
+    def decodeSpell(self,x):
+        trigrams = []
+        trigrams.append('_'+x[0:2])
         for i in range(1,len(x)-1):
-            trigrama.append(x[i-1:i+2])
-        trigrama.append(x[len(x)-2:len(x)] + '_')
+            trigrams.append(x[i-1:i+2])
+        trigrams.append(x[len(x)-2:len(x)] + '_')
 
         spells = []
-        for j in range(len(self.trigramas)):
+        for j in range(len(self.trigramss)):
             contador = 0
             for i in range(5):
-                if i < len(trigrama) and self.trigramas[j][i] == trigrama[i]:
+                if i < len(trigrams) and self.trigramss[j][i] == trigrams[i]:
                     contador+=1
             spells.append(contador)
         
-        #print("mais proxima é sem duvidas: ", self.magias[spells.index(max(spells))])
+        #print("mais proxima é sem duvidas: ", self.spells[spells.index(max(spells))])
         return spells.index(max(spells))
 
-    def compara(self, magia1, magia2):
+    def compara(self, spell1, spell2):
         CRITICO = 10
-        dano = 0
-        if magia1.confidence > magia2.confidence:
-            if magia1.attack and magia2.defesa:
-                dano += magia1.confidence - magia2.confidence
-            elif magia1.attack and magia2.attack:
-                dano += magia1.confidence
-            elif magia1.defesa and magia2.defesa:
-                dano = 0
-            elif magia1.defesa and magia2.attack:
-                dano += magia1.confidence - magia2.confidence
-            #if magia1.element > magia2.element:
-            #            dano += CRITICO
-            return dano, 0
-        elif magia2.confidence > magia1.confidence:
-            if magia2.attack and magia2.defesa:
-                dano += magia2.confidence - magia1.confidence
-            elif magia2.attack and magia1.attack:
-                dano += magia2.confidence
-            elif magia2.defesa and magia1.defesa:
-                dano = 0
-            elif magia2.defesa and magia1.attack:
-                dano += magia2.confidence - magia1.confidence
-            #if magia2.element > magia2.element:
-            #    dano += CRITICO
-            return 0, dano
+        damage = 0
+        if spell1.confidence > spell2.confidence:
+            if spell1.attack and spell2.defense:
+                damage += spell1.confidence - spell2.confidence
+            elif spell1.attack and spell2.attack:
+                damage += spell1.confidence
+            elif spell1.defense and spell2.defense:
+                damage = 0
+            elif spell1.defense and spell2.attack:
+                damage += spell1.confidence - spell2.confidence
+            #if spell1.element > spell2.element:
+            #            damage += CRITICO
+            return damage, 0
+        elif spell2.confidence > spell1.confidence:
+            if spell2.attack and spell2.defense:
+                damage += spell2.confidence - spell1.confidence
+            elif spell2.attack and spell1.attack:
+                damage += spell2.confidence
+            elif spell2.defense and spell1.defense:
+                damage = 0
+            elif spell2.defense and spell1.attack:
+                damage += spell2.confidence - spell1.confidence
+            #if spell2.element > spell2.element:
+            #    damage += CRITICO
+            return 0, damage
 
+        return 0, 0
     #envio para os clients a vida atual dos dois players e o buffedLetter e a debuffedLetter
     #def send(self,buffedLetter,debuffedLetter,healthPoints1,healthPoints2):
-    #   send this to both clients    
-    
-    def turn(self):
-        
+    #   send this to both clients 
+
+
+    def start_turn(self):
+        #send(buffedLetter, debuffedLetter, self.player1.healthPoints, self.player2.healthPoints)
         working_letters = ['A','B','C','D','E','I','L','U','W']
         buffedLetter = random.choice(working_letters)
         debuffedLetter = random.choice(working_letters)
-        
-        
-        #send(buffedLetter, debuffedLetter, self.player1.healthPoints, self.player2.healthPoints)
-        
+        return (buffedLetter, debuffedLetter)
+    
+    def end_turn(self, spellFromClient1, spellFromClient2):
         #espero na porta por TIMEOUT segundos; devo receber uma string e um confidence
 
-        #transformar as strings recebidas em magias, criar 
-        self.player1.setSpell(decodificaSpell(spellFromClient1))
-        self.player2.setSpell(decodificaSpell(spellFromClient2))
-
-        dano1, dano2 = compara(magia1,magia2)
-
-        self.player1.updateHealthPoints(dano1)
-        self.player2.updateHealthPoints(dano2)
-
-
+        #transformar as strings recebidas em spells, criar 
+        s1Index = self.decodeSpell(spellFromClient1.element)
+        s2Index = self.decodeSpell(spellFromClient2.element)
+        self.player1.setSpell(s1Index)
+        self.player2.setSpell(s2Index)
+        spell1 = Spell(spellFromClient1.confidence, spellFromClient1.tipo, self.spells[s1Index])
+        spell2 = Spell(spellFromClient2.confidence, spellFromClient2.tipo, self.spells[s2Index])
         
+        damage1, damage2 = self.compara(spell1,spell2)
+        self.player1.updateHealthPoints(damage1)
+        self.player2.updateHealthPoints(damage2)
 
-
-
-
-        
+        return (self.player1.healthPoints, self.player2.healthPoints)
---
+++ Servidor/server.py
@@ -0,0 +1,82 @@
+import os
+import json
+from Game import Game, Magia
+from flask import Flask, render_template, request, session
+from flask_socketio import SocketIO, join_room, leave_room, send, emit
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'quem adivinhar é gay'
+socketio = SocketIO(app, cors_allowed_origins="*", cors_allowed_methods ='*',  cors_allowed_headers='*')
+
+matching = ''
+games = {}
+users = {}
+
+def add_game(player1, player2):
+    print('Starting new game!')
+    print('Player1: {}, Player2: {}'.format(player1,player2))
+    gameID = player1 + player2
+    users[player1] = {'enemy': player2, 'first': True, 'spell': ''}
+    users[player2] = {'enemy': player1, 'first': False, 'spell': ''}
+    games[gameID] = Game(player1, player2)
+    print(f'Number of users: {len(users)}')
+    print(f'Number of games: {len(games)}')
+
+@socketio.on('connect')
+def connect():
+    global matching
+    if matching == '':
+        matching = request.sid
+        print('User {matching} is waiting for an oponent!')
+    else:
+        add_game(matching, request.sid)
+        matching = ''
+
+@socketio.on('disconnect')
+def disconnect():
+    global matching
+    playerID = request.sid
+    print(f'Disconnecting user {playerID}')
+    if users[playerID]['enemy']:
+        enemyID = users[playerID]['enemy']
+        gameID = playerID + enemyID if users[playerID]['first'] else enemyID+playerID 
+        users.pop(playerID)
+        games.pop(gameID)
+        if matching == '':
+            matching = enemyID
+        else:
+            add_game(matching, enemyID)
+            matching = ''
+    else:
+        users.pop(playerID)
+
+    print(f'Number of users: {len(users)}')
+    print(f'Number of games: {len(games)}')
+
+@socketio.on('end_turn')
+def end_turn(data):
+    playerID = request.sid
+    enemyID = users[playerID]['enemy']
+    gameID = playerID + enemyID if users[playerID]['first'] else enemyID+playerID
+    print('playerID: {}, enemyID: {}, gameID: {}'.format(playerID, enemyID, gameID))
+
+    users[playerID]['spell'] = Magia(int(data['confidence']), data['type'], data['element'])
+    if users[enemyID]['spell'] != '':
+        if not users[playerID]['first']:
+            temp = playerID
+            playerID = enemyID
+            enemyID = temp
+
+        spell1 = users[playerID]['spell']
+        spell2 = users[enemyID]['spell']
+        playerHP, enemyHP = games[gameID].end_turn(spell1, spell2)
+        emit('update_hp', json.dumps({'player':playerHP, 'enemy':enemyHP}), to=playerID)
+        emit('update_hp', json.dumps({'player':enemyHP, 'enemy':playerHP}), to=enemyID)
+        
+        spell1 = users[enemyID]['spell'] = ''
+        spell2 = users[playerID]['spell'] = ''
+
+if __name__ == '__main__':
+    PORT = int(os.environ.get("PORT", 5000))
+    print(f'abriu na porta {PORT}')
+    socketio.run(app, host='0.0.0.0', port=PORT)
+    
